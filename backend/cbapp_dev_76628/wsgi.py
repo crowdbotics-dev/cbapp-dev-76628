7085b497cfb8017ba645fef6b184bdbfbd3f814f
Crowdbotics <engineering@crowdbotics.com>
2023-04-14 06:18:00 +0000

    Initial commit.

---
+++ backend/cbapp_dev_76628/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for cbapp_dev_76628 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cbapp_dev_76628.settings')
+
+application = get_wsgi_application()
